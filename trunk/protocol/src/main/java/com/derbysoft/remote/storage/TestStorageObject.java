2be89bf7a99adcf75e7c739867dc7ec5f1a9b4b7
Java2Enterprise <Java2Enterprise@92508e05-def2-8eaf-238e-de461130b210>
2010-04-06 09:57:51 +0000

    Intialiazed package structure.

---
+++ trunk/protocol/src/main/java/com/derbysoft/remote/storage/TestStorageObject.java
@@ -0,0 +1,9 @@
+package com.derbysoft.remote.storage;
+
+import com.derbysoft.remote.share.dto.AbstractDTO;
+
+public class TestStorageObject extends AbstractDTO {
+
+	private static final long serialVersionUID = 2800812095658634513L;
+
+}
---
+++ trunk/storage/src/main/java/com/derbysoft/storage/storage/StorageMain.java
@@ -0,0 +1,9 @@
+package com.derbysoft.storage.storage;
+
+public class StorageMain {
+
+	public static void main(String[] args) {
+		System.out.println("Storage");
+	}
+
+}
